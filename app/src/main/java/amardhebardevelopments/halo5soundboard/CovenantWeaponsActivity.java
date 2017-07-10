b12428911412a0f690cb866729954b7f3c729dcb
Amar Dhebar <amar.dhebar.ad@gmail.com>
2017-07-10 14:19:20 -0500

    Added initial set of sounds for Covenant and finished first page for Covenant weps

--- app/src/main/java/amardhebardevelopments/halo5soundboard/CovenantWeaponsActivity.java
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/CovenantWeaponsActivity.java
@@ -1,7 +1,10 @@
 package amardhebardevelopments.halo5soundboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CovenantWeaponsActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class CovenantWeaponsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covenant_weapons);
+        ImageButton carbine = (ImageButton) findViewById(R.id.carbine);
+        ImageButton beamRifle = (ImageButton) findViewById(R.id.beamrifle);
+        ImageButton energySword = (ImageButton) findViewById(R.id.energysword);
+
+        carbine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity.this, R.raw.h5_carbine_3);
+                mp.start();
+            }
+        });
+
+        beamRifle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity.this, R.raw.h5_beam_1);
+                mp.start();
+            }
+        });
+
+        energySword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity.this, R.raw.h5_e_sword_swing_object_impact_3);
+                mp.start();
+            }
+        });
     }
 }
