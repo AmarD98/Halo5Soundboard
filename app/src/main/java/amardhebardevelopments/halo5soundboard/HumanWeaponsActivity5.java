7eb9f95860083479cbc79c6d8abc0fe0f71177c0
Amar Dhebar <amar.dhebar.ad@gmail.com>
2017-07-10 18:10:55 -0500

    Made background for intro screen and finished backend work of human weps page 5

--- app/src/main/java/amardhebardevelopments/halo5soundboard/HumanWeaponsActivity5.java
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/HumanWeaponsActivity5.java
@@ -1,18 +1,13 @@
 package amardhebardevelopments.halo5soundboard;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class HumanWeaponsActivity5 extends AppCompatActivity {
 
-    private GestureDetectorCompat gestureObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,39 +40,10 @@ public class HumanWeaponsActivity5 extends AppCompatActivity {
                 mp.start();
             }
         });
-        gestureObject = new GestureDetectorCompat(this, new MyGestureDetector5());
-
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        this.gestureObject.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
-}
 
 
-class MyGestureDetector5 extends GestureDetector.SimpleOnGestureListener{
-
-
-    @Override
-    public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY){
-
-        if(event1.getX() > event2.getX()) //Right to left swipe
-        {
-
-        }
-        else
-        if(event2.getX() > event1.getX())//Left to right swipe
-        {
-            Intent intent = new Intent(new HumanWeaponsActivity5(),
-                    HumanWeaponsActivity4.class);
-            new HumanWeaponsActivity5().getApplicationContext().startActivity(intent);
-
-        }
-
-
-        return true;
     }
 
 }
+
+
