78e2073245e11f2bd18d03ecab81173607b62cfd
Amar Dhebar <amar.dhebar.ad@gmail.com>
2017-07-11 02:11:56 -0500

    Finished cov weps page 2 and started work on third page, added missing images that did not come with original icon pack

--- app/src/main/java/amardhebardevelopments/halo5soundboard/CovenantWeaponsActivity2.java
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/CovenantWeaponsActivity2.java
@@ -1,8 +1,66 @@
 package amardhebardevelopments.halo5soundboard;
 
-/**
- * Created by Electrania.com on 7/10/2017.
- */
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class CovenantWeaponsActivity2 {
+public class CovenantWeaponsActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covenant_weapons2);
+
+        ImageButton plasmaCaster = (ImageButton) findViewById(R.id.plasmaCaster);
+        ImageButton plasmaPistol = (ImageButton) findViewById(R.id.plasmaPistol);
+        ImageButton fuelRodCannon = (ImageButton) findViewById(R.id.fuelRod);
+
+        plasmaCaster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity2.this, R.raw.h5_p_caster_reg_2);
+                mp.start();
+            }
+        });
+
+        plasmaPistol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity2.this, R.raw.h5_p_pistol_reg_1);
+                mp.start();
+            }
+        });
+
+        fuelRodCannon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(CovenantWeaponsActivity2.this, R.raw.h5_fuel_rod_2);
+                mp.start();
+            }
+        });
+
+        Button covPage2Right = (Button) findViewById(R.id.covPage2Right);
+        Button covPage2Left = (Button) findViewById(R.id.covPage2Left);
+
+        covPage2Left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goLeft = new Intent(view.getContext(), CovenantWeaponsActivity.class);
+                startActivity(goLeft);
+            }
+        });
+
+        covPage2Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goRight = new Intent(view.getContext(), CovenantWeaponsActivity3.class);
+                startActivity(goRight);
+            }
+        });
+
+    }
 }
---
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/CovenantWeaponsActivity3.java
@@ -0,0 +1,15 @@
+package amardhebardevelopments.halo5soundboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CovenantWeaponsActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covenant_weapons3);
+
+
+    }
+}
