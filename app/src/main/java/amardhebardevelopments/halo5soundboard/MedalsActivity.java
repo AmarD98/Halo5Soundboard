a99b26215f16af46edbc07f38cecee9b00ce1c17
Amar Dhebar <amar.dhebar.ad@gmail.com>
2017-07-12 17:01:42 -0500

    Finished adding sounds for chain medals, and added ad banner for chain medals page 1.

--- app/src/main/java/amardhebardevelopments/halo5soundboard/MedalsActivity.java
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/MedalsActivity.java
@@ -1,7 +1,13 @@
 package amardhebardevelopments.halo5soundboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MedalsActivity extends AppCompatActivity {
 
@@ -9,6 +15,93 @@ public class MedalsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medals);
+
+        AdView adView = (AdView) findViewById(R.id.adViewMedalsPage1);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        ImageButton doubleKill = (ImageButton) findViewById(R.id.doubleKill);
+        ImageButton tripleKill = (ImageButton) findViewById(R.id.tripleKill);
+        ImageButton overKill = (ImageButton) findViewById(R.id.overKill);
+        ImageButton killtacular = (ImageButton) findViewById(R.id.overKill);
+        ImageButton killtrocity = (ImageButton) findViewById(R.id.killtrocity);
+        ImageButton killimanjaro = (ImageButton) findViewById(R.id.killimanjaro);
+        ImageButton killtastrophe = (ImageButton) findViewById(R.id.killtastrophe);
+        ImageButton killpocalypse = (ImageButton) findViewById(R.id.killpocalypse);
+        ImageButton killionaire = (ImageButton) findViewById(R.id.killionaire);
+
+        doubleKill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.doublekill);
+                mp.start();
+            }
+        });
+
+        tripleKill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.triplekill);
+                mp.start();
+            }
+        });
+
+        overKill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.overkill);
+                mp.start();
+            }
+        });
+
+        killtacular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killtacular);
+                mp.start();
+            }
+        });
+
+        killtrocity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killtrocity);
+                mp.start();
+            }
+        });
+
+        killimanjaro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killimanjaro);
+                mp.start();
+            }
+        });
+
+        killtastrophe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killtastrophe);
+                mp.start();
+            }
+        });
+
+        killpocalypse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killpocalypse);
+                mp.start();
+            }
+        });
+
+        killionaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mp = MediaPlayer.create(MedalsActivity.this,  R.raw.killionaire);
+                mp.start();
+            }
+        });
+
 
     }
 }
