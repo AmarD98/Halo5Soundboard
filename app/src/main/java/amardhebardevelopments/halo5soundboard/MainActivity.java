93c84ffca4f5cf9b676f13db2c580ec11f917a9a
Amar Dhebar <amar.dhebar.ad@gmail.com>
2017-07-12 17:45:59 -0500

    Removed extra image files.
     Added ad banner to main page and added color to wep and medal buttons on main page.

--- app/src/main/java/amardhebardevelopments/halo5soundboard/MainActivity.java
+++ app/src/main/java/amardhebardevelopments/halo5soundboard/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,11 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AdView adView = (AdView) findViewById(R.id.adViewMainPage);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
         Button weapons = (Button) findViewById(R.id.button);
         weapons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext() , WeaponsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button medals = (Button) findViewById(R.id.medals);
+        medals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MedalsActivity.class);
                 startActivity(intent);
             }
         });
